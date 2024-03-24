2f209197247496a4a0598d5fae15cb40169c8707
511172176 <511172176@m365.fju.edu.tw>
2024-03-25 00:38:25 +0800

    update

--- app/src/main/java/edu/fju/minesweeper_app/MainActivity.kt
+++ app/src/main/java/edu/fju/minesweeper_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package edu.fju.minesweeper_app
+package edu.fju.minesweeper_app;
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -11,10 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
