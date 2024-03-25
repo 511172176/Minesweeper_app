e6755692bdd7b4f22a829a4e33f7447dff06063e
511172176 <511172176@m365.fju.edu.tw>
2024-03-25 16:26:22 +0800

    update

--- app/src/main/java/edu/fju/minesweeper_app/MainActivity.kt
+++ app/src/main/java/edu/fju/minesweeper_app/MainActivity.kt
@@ -1,17 +1,168 @@
-package edu.fju.minesweeper_app;
+package edu.fju.minesweeper_app
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
+// 定義 MainActivity，繼承自 AppCompatActivity 進行 UI 控制
 class MainActivity : AppCompatActivity() {
+
+    // 定義遊戲所需的變量和控件參考
+    lateinit var rcAdapter: RcAdapter // 適配器，用於控制 RecyclerView 的數據和表現
+    lateinit var flagNum: TextView // 顯示剩餘標記數量的 TextView
+    lateinit var time: TextView // 顯示計時器時間的 TextView
+    lateinit var reset: TextView // 觸發重置遊戲的 TextView
+
+    private var time1 = 0L // 計時器的分鐘數
+    private var time2 = 0L // 計時器的秒數
+
+    private val handler: Handler = Handler() // 用於實現計時功能的 Handler
+
+    // 勝利彈框的延遲初始化
+    private lateinit var boxDialog: BoxDialog
+
+    // 計時器，每秒更新一次時間顯示
+    private val mCounter: Runnable = object : Runnable {
+        @SuppressLint("SetTextI18n")
+        override fun run() {
+            handler.postDelayed(this, 1000)
+            time2++
+            if (60L == time2) {
+                time1++
+                time2 = 0
+            }
+            // 更新時間顯示，格式化為 00:00
+            time.text =
+                (if (time1 < 10) "0$time1" else time1.toString()) + ":" + if (time2 < 10) "0$time2" else time2.toString()
+        }
+    }
+
+    @SuppressLint("SetTextI18n", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // 狀態欄設置
+        val titleBar = findViewById<LinearLayout>(R.id.titleBar)
+        StatusBarUtil.immersive(this)
+        StatusBarUtil.darkMode(this)
+        StatusBarUtil.setPaddingSmart(this, titleBar)
 
+        // 控件實例化
+        flagNum = findViewById(R.id.flagNum)
+        time = findViewById(R.id.time)
+        reset = findViewById(R.id.reset)
+
+        // 加載遊戲布局
+        initView()
+
+        // 重置點擊事件設置
+        reset.setOnClickListener {
+            // 重新創建遊戲
+            MinefieldUtil.reset()
+
+            // 計時器重置
+            handler.removeCallbacks(mCounter)
+            time.text = "00:00"
+
+            // 小紅旗重置
+            flagNum.text = "--"
+
+            // 列表刷新
+            rcAdapter.notifyDataSetChanged()
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun initView() {
+        val rc = findViewById<RecyclerView>(R.id.rc)
+        val layoutParams = rc.layoutParams
+        val pxValue = dip2px(this, 38F)
+        layoutParams.width = pxValue * 9
+        layoutParams.height = pxValue * 16
+
+        rc.layoutManager = GridLayoutManager(this, 9)
+        rcAdapter = RcAdapter(this)
+        rc.adapter = rcAdapter
+
+        rcAdapter.setDataCallBack(object : RcAdapter.DataCallBack {
+            // 遊戲開始
+            override fun gameStart() {
+                time1 = 0
+                time2 = 0
+                handler.post(mCounter)
+            }
+
+            // 遊戲結束
+            override fun gameOver() {
+                MinefieldUtil.isEstablish = false
+                flagNum.text = "--"
+
+                // 停止計時
+                handler.removeCallbacks(mCounter)
+            }
+
+            // 使用小紅旗
+            override fun useFlag() {
+                if (MinefieldUtil.isEstablish) {
+                    MinefieldUtil.flagNum--
+                    flagNum.text = MinefieldUtil.flagNum.toString()
+                }
+            }
+
+            // 取消使用小紅旗
+            override fun cancelFlag() {
+                if (MinefieldUtil.isEstablish) {
+                    MinefieldUtil.flagNum++
+                    flagNum.text = MinefieldUtil.flagNum.toString()
+                }
+            }
+
+            // 遊戲勝利
+            @SuppressLint("SetTextI18n")
+            override fun gameWins() {
+                // 停止計時器
+                handler.removeCallbacks(mCounter)
+
+                // 顯示勝利彈窗
+                val inflate: View = LayoutInflater.from(this@MainActivity)
+                    .inflate(R.layout.dialog_win, null, false)
+                val consume = inflate.findViewById<TextView>(R.id.consume)
+
+                // 顯示用時
+                consume.text = "用时：" + (if (time1 < 10) "0$time1" else time1.toString()) + ":" + if (time2 < 10) "0$time2" else time2.toString()
+
+                // 再來一局按鈕設定
+                val again = inflate.findViewById<TextView>(R.id.again)
+                again.setOnClickListener {
+                    // 重新開始遊戲
+                    time1 = 0
+                    time2 = 0
+                    MinefieldUtil.reset()
+                    rcAdapter.notifyDataSetChanged()
+                    boxDialog.dismiss()
+                }
+
+                // 顯示彈窗
+                boxDialog = BoxDialog(this@MainActivity, inflate, BoxDialog.LocationView.CENTER)
+                boxDialog.setCancelable(false) // 是否可以點擊彈窗外關閉彈窗
+                boxDialog.setCanceledOnTouchOutside(false) // 是否可以按返回鍵關閉彈窗
+                boxDialog.show()
+            }
+        })
+    }
+
+    // dp轉px的工具方法
+    private fun dip2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
     }
 }
---
+++ app/src/main/java/edu/fju/minesweeper_app/RcAdapter.kt
@@ -0,0 +1,333 @@
+package com.cyn.traps
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class RcAdapter(private var context: Context) : RecyclerView.Adapter<RcAdapter.Holder>() {
+
+    //當遊戲失敗後，失敗處的座標，此處要著重顯示
+    private var overPosition = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+
+        return Holder(LayoutInflater.from(context).inflate(R.layout.item_lattice, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return 480
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onBindViewHolder(holder: Holder, @SuppressLint("RecyclerView") position: Int) {
+        val d = position / 9
+        val k = position - 9 * d
+
+        //-1：地雷區域
+        //0-8：周圍地雷數量
+        val indexGame = MinefieldUtil.gameMap[d][k]
+
+        //0：未開採
+        //1：已開踩
+        //2：標記小紅旗
+        //3：問號
+        val indexOperation = MinefieldUtil.operationMap[d][k]
+
+        //判斷是否公開雷區
+        if (MinefieldUtil.isOpen) {
+            //公開雷區，遊戲結束
+
+            when (indexOperation) {
+                0, 3 -> {
+                    if (indexGame == -1) {
+                        holder.itemText.setBackgroundResource(R.mipmap.icon_trap_open)
+                        holder.itemText.text = ""
+                    } else {
+                        holder.itemText.setBackgroundResource(R.mipmap.icon_lattice)
+                        holder.itemText.text = ""
+                    }
+                }
+                1 -> {
+                    holder.itemText.setBackgroundResource(R.mipmap.icon_empty)
+                    holder.itemText.text = indexGame.toString()
+
+                    if (0 != indexGame) {
+                        holder.itemText.text = indexGame.toString()
+                    } else {
+                        holder.itemText.text = ""
+                    }
+
+                    holder.itemText.setTextColor(
+                        when (indexGame) {
+                            1 -> ContextCompat.getColor(context, R.color.index1)
+                            2 -> ContextCompat.getColor(context, R.color.index2)
+                            3 -> ContextCompat.getColor(context, R.color.index3)
+                            4 -> ContextCompat.getColor(context, R.color.index4)
+                            5 -> ContextCompat.getColor(context, R.color.index5)
+                            6 -> ContextCompat.getColor(context, R.color.index6)
+                            7 -> ContextCompat.getColor(context, R.color.index7)
+                            else -> ContextCompat.getColor(context, R.color.index8)
+                        }
+                    )
+                }
+                2 -> {
+                    if (indexGame == -1) {
+                        holder.itemText.setBackgroundResource(R.mipmap.icon_flag)
+                        holder.itemText.text = ""
+                    } else {
+                        holder.itemText.setBackgroundResource(R.mipmap.icon_flag_error)
+                        holder.itemText.text = ""
+                    }
+                }
+            }
+
+            if (indexOperation == 0 && -1 == indexGame) {
+                holder.itemText.setBackgroundResource(R.mipmap.icon_trap_open)
+                holder.itemText.text = ""
+            }
+
+            if (overPosition == position) {
+                holder.itemText.setBackgroundResource(R.mipmap.icon_trap)
+                holder.itemText.text = ""
+            }
+
+        } else {
+            //隱藏雷區
+            when (indexOperation) {
+                0 -> {
+                    holder.itemText.setBackgroundResource(R.mipmap.icon_lattice)
+                    holder.itemText.text = ""
+                    holder.itemText.setOnClickListener {
+                        //開採區域
+                        if (-1 == indexGame) {
+                            //踩到地雷，遊戲結束
+                            MinefieldUtil.isOpen = true
+                            overPosition = position
+                            notifyDataSetChanged()
+                            dataCallBack?.gameOver()
+                        } else {
+
+//                            dataCallBack?.gameWins()
+
+                            //回調遊戲開始
+                            if (!MinefieldUtil.isEstablish) {
+                                dataCallBack?.gameStart()
+                            }
+
+                            //本次點擊排除一個格子
+                            MinefieldUtil.turnedOnNum++
+
+                            //創建地雷，本局遊戲只會執行一次，內部已封裝好方法
+                            MinefieldUtil.establish(d, k)
+
+                            //遞歸開採其他模組
+                            exploitation(d, k)
+
+
+                            //判斷是否已經排除完地雷
+                            if (381 == MinefieldUtil.turnedOnNum) {
+                                dataCallBack?.gameWins()
+                            }
+
+                            //刷新
+                            notifyDataSetChanged()
+
+                        }
+                    }
+                    holder.itemText.setOnLongClickListener {
+                        //在該區域插上小紅旗
+
+                        //判斷小紅旗是否用完了
+                        if (MinefieldUtil.flagNum <= 0) {
+                            return@setOnLongClickListener true
+                        }
+
+                        MinefieldUtil.operationMap[d][k] = 2
+
+                        //回調使用了小紅旗
+                        dataCallBack?.useFlag()
+
+                        notifyDataSetChanged()
+                        return@setOnLongClickListener true
+                    }
+                }
+
+                1 -> {
+                    if (0 == indexGame) {
+                        //已開採周圍沒有地雷的方塊
+                        holder.itemText.setBackgroundResource(R.mipmap.icon_empty)
+                        holder.itemText.text = ""
+                    } else {
+                        //已開採周圍有地雷的方塊
+                        holder.itemText.setBackgroundResource(R.mipmap.icon_empty)
+                        holder.itemText.text = indexGame.toString()
+                        holder.itemText.setTextColor(
+                            when (indexGame) {
+                                1 -> ContextCompat.getColor(context, R.color.index1)
+                                2 -> ContextCompat.getColor(context, R.color.index2)
+                                3 -> ContextCompat.getColor(context, R.color.index3)
+                                4 -> ContextCompat.getColor(context, R.color.index4)
+                                5 -> ContextCompat.getColor(context, R.color.index5)
+                                6 -> ContextCompat.getColor(context, R.color.index6)
+                                7 -> ContextCompat.getColor(context, R.color.index7)
+                                else -> ContextCompat.getColor(context, R.color.index8)
+                            }
+                        )
+                    }
+                }
+
+                2 -> {
+                    holder.itemText.setBackgroundResource(R.mipmap.icon_flag)
+                    holder.itemText.text = ""
+                    holder.itemText.setOnLongClickListener {
+
+                        MinefieldUtil.operationMap[d][k] = 3
+
+                        dataCallBack?.cancelFlag()
+
+                        notifyDataSetChanged()
+
+
+
+                        return@setOnLongClickListener true
+                    }
+                }
+
+                3 -> {
+                    holder.itemText.setBackgroundResource(R.mipmap.icon_doubt)
+                    holder.itemText.text = ""
+                    holder.itemText.setOnLongClickListener {
+                        MinefieldUtil.operationMap[d][k] = 0
+                        notifyDataSetChanged()
+                        return@setOnLongClickListener true
+                    }
+                }
+            }
+        }
+    }
+
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemText: TextView = itemView.findViewById(R.id.itemText)
+    }
+
+
+    //==============================================================================================
+    /**
+     * 開採領域，遞迴調用
+     */
+    private fun exploitation(d: Int, k: Int) {
+        if (MinefieldUtil.gameMap[d][k] >= 0) {
+            MinefieldUtil.operationMap[d][k] = 1
+
+
+            if (0 != MinefieldUtil.gameMap[d][k]) {
+                return
+            }
+
+            //判斷左側是否開採
+            if (k >= 1 && MinefieldUtil.gameMap[d][k - 1] >= 0 && MinefieldUtil.operationMap[d][k - 1] != 1) {
+                MinefieldUtil.operationMap[d][k - 1] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d][k - 1] == 0) {
+                    exploitation(d, k - 1)
+                }
+            }
+
+            //判斷上側是否開採
+            if (d >= 1 && MinefieldUtil.gameMap[d - 1][k] >= 0 && MinefieldUtil.operationMap[d - 1][k] != 1) {
+                MinefieldUtil.operationMap[d - 1][k] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d - 1][k] == 0) {
+                    exploitation(d - 1, k)
+                }
+            }
+
+            //判斷右側是否開採
+            if (k <= 28 && MinefieldUtil.gameMap[d][k + 1] >= 0 && MinefieldUtil.operationMap[d][k + 1] != 1) {
+                MinefieldUtil.operationMap[d][k + 1] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d][k + 1] == 0) {
+                    exploitation(d, k + 1)
+                }
+            }
+
+            //判斷下側是否開採
+            if (d <= 14 && MinefieldUtil.gameMap[d + 1][k] >= 0 && MinefieldUtil.operationMap[d + 1][k] != 1) {
+                MinefieldUtil.operationMap[d + 1][k] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d + 1][k] == 0) {
+                    exploitation(d + 1, k)
+                }
+            }
+
+            //判斷左上是否開採
+            if (d >= 1 && k >= 1 && MinefieldUtil.gameMap[d - 1][k - 1] >= 0 && MinefieldUtil.operationMap[d - 1][k - 1] != 1) {
+                MinefieldUtil.operationMap[d - 1][k - 1] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d - 1][k - 1] == 0) {
+                    exploitation(d - 1, k - 1)
+                }
+            }
+
+            //判斷右上是否開採
+            if (d >= 1 && k <= 28 && MinefieldUtil.gameMap[d - 1][k + 1] >= 0 && MinefieldUtil.operationMap[d - 1][k + 1] != 1) {
+                MinefieldUtil.operationMap[d - 1][k + 1] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d - 1][k + 1] == 0) {
+                    exploitation(d - 1, k + 1)
+                }
+            }
+
+            //判斷右下是否開採
+            if (d <= 14 && k <= 28 && MinefieldUtil.gameMap[d + 1][k + 1] >= 0 && MinefieldUtil.operationMap[d + 1][k + 1] != 1) {
+                MinefieldUtil.operationMap[d + 1][k + 1] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d + 1][k + 1] == 0) {
+                    exploitation(d + 1, k + 1)
+                }
+            }
+
+            //判斷左下是否開採
+            if (d <= 14 && k >= 1 && MinefieldUtil.gameMap[d + 1][k - 1] >= 0 && MinefieldUtil.operationMap[d + 1][k - 1] != 1) {
+                MinefieldUtil.operationMap[d + 1][k - 1] = 1
+                MinefieldUtil.turnedOnNum++
+                if (MinefieldUtil.gameMap[d + 1][k - 1] == 0) {
+                    exploitation(d + 1, k - 1)
+                }
+            }
+
+        }
+    }
+
+    //==============================================================================================
+    //相關事件回調
+    private var dataCallBack: DataCallBack? = null
+
+    fun setDataCallBack(dataCallBack: DataCallBack) {
+        this.dataCallBack = dataCallBack
+    }
+
+    interface DataCallBack {
+
+        fun gameStart()
+
+        //遊戲結束
+        fun gameOver()
+
+        //使用小紅旗
+        fun useFlag()
+
+        //取消使用小紅旗
+        fun cancelFlag()
+
+        //遊戲勝利
+        fun gameWins()
+    }
+
+
+}
